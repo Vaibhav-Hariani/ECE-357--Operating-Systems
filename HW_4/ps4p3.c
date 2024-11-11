3d5016b09474db51fd3226c44fabe837506c8ff4
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-11-11 01:16:05 -0500

    That's a lot of changes;
    Mostly done with the assignment; need to add signal handlers, and make sure I'm shutting down the two forks if I get a siglongjump.

--- HW_4/ps4p3.c
+++ HW_4/ps4p3.c
@@ -1,92 +1,165 @@
+#define _XOPEN_SOURCE 700
+
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/resource.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
+#include <signal.h>
+#include <setjmp.h>
+
+
+
 extern int errno;
 
-int grep_cmd(int* grep_in, int* grep_out, char* pattern){
-    //
-    if(dup2(grep_in,STDIN_FILENO) < 0){
-        fprintf(stderr, "Could not Complete dup2 for stdin: %s", strerror(errno));
+
+int grep_cmd(int* grep_in, int* grep_out, char* pattern) {
+
+    if (dup2(grep_in, STDIN_FILENO) < 0) {
+        fprintf(stderr, "Could not complete dup2 for stdin to grep: %s",
+                strerror(errno));
         exit(1);
     }
-    if(dup2(grep_out,STDOUT_FILENO) < 0){
-        fprintf(stderr, "Could not Complete dup2 for stdout: %s", strerror(errno));
+    if (dup2(grep_out, STDOUT_FILENO) < 0) {
+        fprintf(stderr, "Could not complete dup2 for stdout to grep: %s",
+                strerror(errno));
         exit(1);
     }
 
     int i = execl("grep", "grep", "pattern", NULL);
-    if(i < 0){
+    if (i < 0) {
         fprintf(stderr, "Exec call failed:  %s", strerror(errno));
         exit(1);
     }
+    return 0;
 }
 
-int more_cmd(int* more_in){
-    //
-    if(dup2(more_in,STDIN_FILENO) < 0){
-        fprintf(stderr, "Could not Complete dup2 for stdin: %s", strerror(errno));
+int more_cmd(int* more_in) {
+
+    if (dup2(more_in, STDIN_FILENO) < 0) {
+        fprintf(stderr, "Could not complete dup2 for stdin to more: %s",
+                strerror(errno));
         exit(1);
     }
     int i = execl("more", "more", NULL);
-    if(i < 0){
+    if (i < 0) {
         fprintf(stderr, "Exec call failed:  %s", strerror(errno));
         exit(1);
     }
+    return 0;
 }
+
+void sigusr1(){
+
+}
+
+// void sigusr2(){
+
+// }
 
 
 int main(int argc, char** argv) {
-    //Pattern is the second argument
+    // Pattern is the second argument
     errno = 0;
     char* pattern = argv[1];
-    for(int i = 2; i < argc; i++){
-        int infile = open(argv[i], O_RDONLY);
-        int grep_i[2];
-        int more_i[2];
 
-        if(errno < 0){
-            fprintf(stderr, "Failed to open file: Reason %s\n", strerror(errno));
+    // Declaring these here for better memory management
+    char buffer[4096];
+    int bufsize = 4096;
+    int* grep_p;
+    int* more_p;
+
+    int infile;
+    int grep;
+    int more;
+
+    int visited_files = 0;
+    int bytes_processed = 0;
+
+    struct sigaction restart;
+    sigemptyset(&restart);
+    restart.sa_flags = SA_RESTART;
+
+    struct sigaction usr1;
+    sigemptyset(&usr1);
+    // usr1.sa_mask = SIGUSR1;
+    
+    sigjmp_buf jmp_buf;
+
+
+    for (int i = 2; i < argc; i++) {
+        // jump here if sigusr2 is sent        
+        int jmp = sigsetjmp(jmp_buf,1);
+        if(jmp != 1) {
+            i++;
+            int sigmask()
+            //Should exit the function if no file left
+            if( i >= argc) {
+                return 0;
+            }
+        }
+
+        infile = open(argv[i], O_RDONLY);
+        visited_files++;
+        grep_p[2];
+        more_p[2];
+
+        if (errno < 0) {
+            fprintf(stderr, "Failed to open file: Reason %s\n",
+                    strerror(errno));
             exit(1);
         }
-        if(pipe(grep_i) < 0 || pipe(more_i) < 0){
+
+        if (pipe(grep_p) < 0 || pipe(more_p) < 0) {
             fprintf(stderr, "Failed to create pipes: %s\n", strerror(errno));
             exit(1);
         }
-        int grep = fork();
-        switch(grep){
-            case 0:
-                //Clean stdin/stdout
-                close(infile);
-                grep_cmd(grep_i,more_i,pattern);
-                break;
-            case -1:
-                fprintf(stderr, "Failed to create grep child: %s\n", strerror(errno));
-                exit(1);
-                break;
-        }
-        //grep will never reach here courtesy of the exit clause
-        //While this might be programatically wrong, I don't want to have nested switch statements        
-        int more = fork();
-        switch(more){
-            case 0:
-                //Clean stdin/stdout
-                close(infile);
-                more_cmd(more_i);
-                break;
-            case -1:
-                fprintf(stderr, "Failed to create grep child: %s\n", strerror(errno));
-                exit(1);
-                break;
-        }
-        
-   
 
+        grep = fork();
+        switch (grep) {
+            case 0:
+                // Clean stdin/stdout
+                close(infile);
+                grep_cmd(grep_p[0], more_p[1], pattern);
+                exit(0);
+                break;
+            case -1:
+                fprintf(stderr, "Failed to create grep child: %s\n",
+                        strerror(errno));
+                exit(1);
+                break;
+        }
+        // grep will never reach here courtesy of the exit
+        // Not to mention that the process is no longer attached to this program
+
+        more = fork();
+        switch (more) {
+            case 0:
+                // Clean stdin/stdout
+                close(infile);
+                more_cmd(more_p[0]);
+                exit(0);
+                break;
+            case -1:
+                fprintf(stderr, "Failed to create more child: %s\n",
+                        strerror(errno));
+                exit(1);
+                break;
+        }
+
+        // Writing to grep_p[1]
+        while (read(infile, buffer, bufsize) == bufsize) {
+            int written = write(grep_p[1], buffer, bufsize);
+            //Keep writing while the number of written bits isn't satisfactory
+            while(written != bufsize){
+                written += write(grep_p[1], buffer + written, bufsize-written);
+            }
+            bytes_processed += written;            
+        }
+        close(infile);
+        close(grep_p[1]);
+
+        wait();
+        wait();
     }
 }
