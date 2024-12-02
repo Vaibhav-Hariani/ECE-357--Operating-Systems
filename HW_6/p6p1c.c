db2453ef63060e6271c56ac0ab090b486616d69c
Vaibhav Hariani <vaibhav.hariani@gmail.com>
2024-12-02 15:26:11 -0500

    added givens, finished HW_6

--- HW_6/p6p1c.c
+++ HW_6/p6p1c.c
@@ -1,23 +1,31 @@
 #define _XOPEN_SOURCE 700
 #include <signal.h>
 #include <stdlib.h>
+#include <tas.h>
 struct ll_head {
     struct ll_elem *first;
+    char* spinlock; //Spinlock
     /* and maybe other stuff */
 };
 struct ll_elem {
+    char* spinlock; //Spinlock
     struct ll_elem *fwd;
     /* and other stuff, such as the payload */
 };
 void ll_insert(struct ll_head *head, struct ll_elem *where,
                struct ll_elem *what) {
+    while(TAS(&what -> spinlock) != 0);
 
     if (where) {
+        while (TAS(&where->spinlock) !=0);
         what->fwd = where->fwd;
         where->fwd = what;
-    } else {
+        where -> spinlock = 0;
+    } else { 
+        while (TAS(&head->spinlock) !=0);
         what->fwd = head->first;
         head->first = what;
+        head->spinlock= 0;
     }
-
+    what -> spinlock = 0;
 }
