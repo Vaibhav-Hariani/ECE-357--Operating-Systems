ac5bde5e65e330d681e33fc6d46a31fff15ecdb8
vaibhav.hariani <vaibhav.hariani@gmail.com>
2024-12-06 11:18:14 -0500

    Finished assignment

--- HW_6/p2/sem.c
+++ HW_6/p2/sem.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "spinlock.h"
+#include<stdio.h>
 
 void sem_init(struct sem *s, int count) {
     s->count = count;
@@ -14,17 +15,16 @@ void sem_init(struct sem *s, int count) {
     for(int i = 0; i < MAX_PROCS; i++){
         s->IDs[i] = 0;
     }
-    spin_unlock(s->lock);
 }
-
+//DOES NOT LOCK: LOCKING NEEDS TO BE HANDLED BEFORE
 int sem_try(struct sem *s) {
-    spin_lock(s->lock);
-    if (s > 0) {
-        s--;
-        spin_unlock(s->lock);
+    // spin_lock(&(s->lock));
+    if (s->count > 0) {
+        s->count--;
+        // spin_unlock(&(s->lock));
         return 0;
     }
-    spin_unlock(s->lock);
+    // spin_unlock(&(s->lock));
     return 1;
 }
 
@@ -34,28 +34,35 @@ void sem_wait(struct sem *s, int vproc) {
     sigemptyset(&full_mask);
     sigemptyset(&suspend_mask);
     sigaddset(&full_mask,SIGUSR1);
-    sigemptyset(&suspend_mask);
+    sigprocmask(SIG_BLOCK, &full_mask, NULL);
+    spin_lock(&(s->lock));
     while (sem_try(s) != 0) {
-        spin_lock(s->lock);
-        sigprocmask(SIG_BLOCK, &full_mask, NULL);
+        // spin_lock(&(s->lock));
         s->IDs[vproc] = getpid();
         s->instrumentation[vproc].sleep++;
         s->proc_count++;
-        spin_unlock(s->lock);
+        spin_unlock(&(s->lock));
         sigsuspend(&suspend_mask);
-        s->instrumentation[vproc].wake++;
+        //Should not be necessary as each process manages its own instrumentation[vproc]
+        spin_lock(&(s->lock));
+        s->instrumentation[vproc].woken++;
+        // spin_unlock(&(s->lock));
     }
+    spin_unlock(&(s->lock));
 }
 
 void sem_inc(struct sem *s){
-    spin_lock(s->lock);
+    spin_lock(&(s->lock));
+    int val = s->count; 
     s->count++;
-    if(s->count > 0){
+    if(val > 0){
         for(int i = 0; i < MAX_PROCS; i++){
-            if(s->IDs[i] == 1){
+            if(s->IDs[i] != 0){
                 kill(s->IDs[i],SIGUSR1);
+                s->IDs[i] = 0;
             }
         }
+        s->proc_count = 0;
     }
-    spin_unlock(s->lock);
+    spin_unlock(&(s->lock));
 }
--- HW_6/p2/shell_game.c
+++ HW_6/p2/shell_game.c
@@ -1,30 +1,89 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "sem.h"
-
+#define NSHELLS 3
 int my_procnum;
+int sig_counter;
 
+void sigusr1_handler(int signo) { sig_counter++; }
+
+void game(struct sem* start, struct sem* end, int num_moves) {
+  fprintf(stderr, "VCPU %d process started: pid %d \n", my_procnum, getpid());
+  for (int i = 0; i < num_moves; i++) {
+    sem_wait(start, my_procnum);
+    sem_inc(end);
+  }
+  fprintf(stderr, "Child %d (pid %d) done: signal handler invoked %d times \n",
+          my_procnum, getpid(), sig_counter);
+}
+
+// Built this to be as general as possible
 int main(int argc, char** argv) {
-    if (argc < 3) {
-        printf("NOT ENOUGH ARGUMENTS\n");
-        return -1;
+  if (argc < 3) {
+    printf("NOT ENOUGH ARGUMENTS\n");
+    return -1;
+  }
+  signal(SIGUSR1, sigusr1_handler);
+  // Parent gets -1
+  my_procnum = -1;
+  int sig_counter = 0;
+  // Not doing any error handling here
+  int num_pebbles = atoi(argv[1]);
+  int num_moves = atoi(argv[2]);
+  // 3 distinct shells
+  struct sem* shell_A =
+      mmap(0, sizeof(struct sem) * NSHELLS, PROT_READ | PROT_WRITE,
+           MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+  struct sem* shell_B = shell_A + 1;
+  struct sem* shell_C = shell_A + 2;
+  // struct sem* shells[] = {shell_A, shell_B, shell_C};
+  for (int i = 0; i < NSHELLS; i++) {
+    sem_init(shell_A + i, num_pebbles);
+  }
+
+  for (int i = 0; i < NSHELLS; i++) {
+    for (int j = i + 1; j < NSHELLS; j++) {
+      int proc = fork();
+      switch (proc) {
+        case 0:
+          my_procnum = 2 * (i + j) - 2;
+          game(shell_A + i, shell_A + j, num_moves);
+          return 0;
+          break;
+        default:
+          break;
+      };
+      // Fork a second process, which makes the opposite moves here
+      proc = fork();
+      switch (proc) {
+        case 0:
+          my_procnum = 2 * (i + j) - 1;
+          game(shell_A + j, shell_A + i, num_moves);
+          return 0;
+          break;
+        default:
+          break;
+      };
     }
-    //Parent gets -1
-    int my_procnum = -1;
-    //Not doing any error handling here
-    int num_pebbles = atoi(argv[1]);
-    int num_moves = atoi(argv[2]);
-    // 3 distinct shells
-    struct sem* shell_A =
-        mmap(0, sizeof(struct sem) * 3, PROT_READ | PROT_WRITE,
-             MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    struct sem* shell_B = shell_A + 1;
-    struct sem* shell_C = shell_A + 2;
-    struct sem* shells[] = {shell_A, shell_B, shell_C};
-    for(int i = 0; i < 3; i++){
-        sem_init(shells[i],num_pebbles);
+  }
+  fprintf(stderr, "Main has spawned all processes: Waiting \n");
+  // Should be NSHELLS factorial
+  for (int i = 0; i < 6; i++) {
+    int childpid = wait(NULL);
+  }
+  fprintf(stderr, "All children complete! Tabling stats \n");
+  fprintf(stderr, "Sem # \t val \t Sleeps \t Wakes \n");
+  for (int i = 0; i < NSHELLS; i++) {
+    struct sem shell = *(shell_A + i);
+    fprintf(stderr, "%d \t %d \n", i, shell.count);
+    for (int i = 0; i < 6; i++) {
+      fprintf(stderr, " VCPU %d \t \t %d \t %d \n", i,
+              shell.instrumentation[i].sleep, shell.instrumentation[i].woken);
     }
-    
+  }
 }
